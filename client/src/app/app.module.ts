93c3f11e76713893dea47ea0fa7a4a24b7a12d3d
chemokita13 <chemokita13@gmail.com>
2022-12-11 22:10:24 +0100

    components added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { GameFormComponent } from './components/game-form/game-form.component';
+import { GameListComponent } from './components/game-list/game-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    GameFormComponent,
+    GameListComponent
   ],
   imports: [
     BrowserModule,
