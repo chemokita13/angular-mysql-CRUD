1664e1a7557a026e858cd33ea2be564c3b4cca73
chemokita13 <chemokita13@gmail.com>
2022-12-11 21:51:33 +0100

    controllers finished

--- server/src/controllers/game.controller.ts
+++ server/src/controllers/game.controller.ts
@@ -3,27 +3,42 @@ import { Request, Response } from "express"; // types by express
 import db from "../db";
 
 class GameController {
-    public async getAll(_req: Request, res: Response) {
+    public async getAll(_req: Request, res: Response): Promise<void> {
         const queryResult = await db.query("SELECT * FROM game");
         res.json(queryResult[0]);
     }
     public async getOne(req: Request, res: Response): Promise<void> {
-        const { id } = req.params;
-        console.log(id);
-        const queryResult = await db.query("SELECT * from game WHERE id = ?", [
-            id,
-        ]);
-        res.json(queryResult[0]);
+        try {
+            const { id } = req.params;
+            console.log(id);
+            const queryResult = await db.query(
+                "SELECT * from game WHERE id = ?",
+                [id]
+            );
+
+            res.json(queryResult[0]);
+        } catch (error) {
+            res.status(404).json({ message: "error, game not exists" });
+        }
     }
     public async newItem(req: Request, res: Response): Promise<void> {
-        await db.query("INSERT INTO game SET ?", [req.body]);
-        res.json("new item saved");
+        const savedItem = await db.query("INSERT INTO game SET ?", [req.body]);
+        res.json(savedItem);
     }
-    public async deleteItem(req: Request, res: Response) {
-        res.json("delete item");
+    public async deleteItem(req: Request, res: Response): Promise<void> {
+        const { id } = req.params;
+        const deletedGame = await db.query("DELETE FROM game WHERE id = ?", [
+            id,
+        ]);
+        res.json(deletedGame);
     }
-    public async updateItem(req: Request, res: Response) {
-        res.json("update item");
+    public async updateItem(req: Request, res: Response): Promise<void> {
+        const { id } = req.params;
+        const updatedGame = await db.query("UPDATE game set ? WHERE id = ?", [
+            req.body,
+            id,
+        ]);
+        res.json(updatedGame);
     }
 }
 
