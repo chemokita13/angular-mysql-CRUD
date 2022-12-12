3dce13fa1c37e519a9b61e3f77dad53da7439788
chemokita13 <chemokita13@gmail.com>
2022-12-12 18:46:06 +0100

    styles and routes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-const routes: Routes = [];
+import { GameListComponent } from "./components/game-list/game-list.component";
+
+const routes: Routes = [
+    {
+        path: "",
+        redirectTo: "/games",
+        pathMatch: "full",
+    },
+    {
+        path: "games",
+        component: GameListComponent,
+    },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
