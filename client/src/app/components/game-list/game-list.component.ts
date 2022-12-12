3872d3177597ccc8bf52692de03db26101509d54
chemokita13 <chemokita13@gmail.com>
2022-12-12 20:24:43 +0100

    styles and fa-icons

--- client/src/app/components/game-list/game-list.component.ts
+++ client/src/app/components/game-list/game-list.component.ts
@@ -2,18 +2,32 @@ import { Component } from "@angular/core";
 
 import { GamesService } from "../../services/games.service";
 
+// import game interface
+import { Game } from "../../models/Game";
+
+import { faPencil, faTrash } from "@fortawesome/free-solid-svg-icons";
+
 @Component({
     selector: "app-game-list",
     templateUrl: "./game-list.component.html",
     styleUrls: ["./game-list.component.css"],
 })
 export class GameListComponent {
+    storedGames: any = []; // I dont know what i did wrong but if i dont put :any code sohws error
+
+    // font awesome icons
+    faPencil = faPencil;
+    faTrash = faTrash;
+
     constructor(private gamesService: GamesService) {}
 
     ngOnInit() {
         // on start page
         this.gamesService.getGames().subscribe({
-            next: (res) => console.log(res),
+            next: (res) => {
+                this.storedGames = res;
+                console.log(this.storedGames);
+            },
             error: (err) => console.log(err),
         });
     }
